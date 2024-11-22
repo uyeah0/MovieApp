0e3aaf393c8aa34d2dafdf69a3ec95a9dc22e85c
uyeah0 <uyeah029@gmail.com>
2024-11-22 15:18:01 +0900

    #2.0 Creating your first React Component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import Potato from "./Potato";
 
-const root = ReactDOM.createRoot(document.getElementById("potato"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.render(<App />, document.getElementById("potato"));
+
+// const root = ReactDOM.createRoot(document.getElementById("potato"));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//     <Potato />
+//   </React.StrictMode>
+// );
+  
